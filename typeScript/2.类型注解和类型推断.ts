78115321a88d4f6b4b4bc65b639366ab7f76da8a
xSalina <419593547@qq.com>
2020-07-30 18:56:21 +0800

    typescript的类型注解、类型推断

--- typeScript/2.类型注解和类型推断.ts
+++ typeScript/2.类型注解和类型推断.ts
@@ -1,7 +1,14 @@
 /*
  * @Date: 2020-07-30 18:32:16
  * @Autor: 肖月萍
- * @Description: Do not edit
+ * @Description: annotation 类型注解   inference 类型推断
  * @LastEditors: 肖月萍
- * @LastEditTime: 2020-07-30 18:32:16
+ * @LastEditTime: 2020-07-30 18:55:40
  */
+
+ //类型注解
+ let count:number;
+ count = 9
+
+
+ const ba = 5;//可以不用注解
